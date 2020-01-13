3722834bda49bda8a8450e11e2f05090f9324a00
charliebp1 <clscrea@gmail.com>
2020-01-13 15:47:18 +0100

    Add files via upload

--- src/app/models/pelicula.ts
+++ src/app/models/pelicula.ts
@@ -1,1 +1,18 @@
+export class Pelicula{
+    
+    /*public title: string;
+      public year: number;
+      public image: string;
 
+    constructor(title, year, image){
+        this.title = title;
+        this.year = year;
+        this.image = image;
+    }*/
+
+    constructor(
+      public title: string,
+      public year: number,
+      public image: string
+    ){}
+}
