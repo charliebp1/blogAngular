51640f87b2288fa9fd53a17ee40dc78b5b28107d
charliebp1 <clscrea@gmail.com>
2020-01-13 15:49:09 +0100

    Add files via upload

--- src/app/pipes/espar.pipe.ts
+++ src/app/pipes/espar.pipe.ts
@@ -1,1 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
 
+@Pipe({
+    name: 'espar'
+})
+
+export class EsParPipe implements PipeTransform{
+
+    transform(value: any){
+        var espar = "no es par";
+        if(value % 2 == 0){
+            espar = "es un número par";
+        }
+
+        return "El año es: "+value+" y "+espar;
+    }
+}
